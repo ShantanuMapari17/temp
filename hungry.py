9d2fa2a8730a3c8de97994a924829b4689e77f7d
Shantanu Mapari <shantanumapari17@gmail.com>
2021-01-21 17:25:35 +0530

    added thirsty input

--- hungry.py
+++ hungry.py
@@ -2,4 +2,8 @@ hungry=input("Are you hungry: ")
 if hungry=="yes":
     print("Eat something!")
 else:
-    print("DO your work!")
+    thirsty=input("Are you thirsty: ")
+    if thirsty=="yes":
+        print("Drink some water!")
+    else:
+        print("DO your work!")
